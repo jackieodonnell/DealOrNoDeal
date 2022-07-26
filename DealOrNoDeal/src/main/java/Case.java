1ba8f1cd48628f43268681b27382c0cd85e32230
Jacqueline ODonnell[D[D[D[D[D[D[D'Donnell[B[A <jodonnellcsg@gmail.com>
2022-07-26 09:02:41 -0400

    setting up DOND game board, player selects first case

---
+++ DealOrNoDeal/src/main/java/Case.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Case {
+    private int caseNumber;
+    private int caseValue;
+    private String display;
+    private boolean isOpen = false;
+
+    public Case(int caseNumber, int caseValue) {
+        this.caseNumber = caseNumber;
+        this.caseValue = caseValue;
+        isOpen = false;
+    }
+
+    public int getCaseNumber() {
+        return caseNumber;
+    }
+
+    public int getCaseValue() {
+        return caseValue;
+    }
+
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
+
+    public String getDisplay(){
+        if (!isOpen){
+            return "[" + caseNumber + "]";
+        } else {
+            return "---";
+        }
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+
+}
